34d460ea88d967b1698ed68fa41eb5bf1d58862a
j-guerreiro <joel.borghi@gmail.com>
2022-04-20 14:00:28 -0300

    feature - estilizando pagina principal e seus elementos

--- index.php
+++ index.php
@@ -3,20 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/estilo.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Loja de Equipamentos Musicais</title>
 </head>
 <body>
-  <header>
+  <header class="cabecalho-principal">
     <!-- formulario de busca -->
     <div class="titulo">
       <h1 class="titulo-interno">Loja de Instrumentos Musicais</h1>
     </div>
     <div class="form-busca">
       <form class="form-interno" method="POST" action="buscar.php">
-        <input type="text">
+        <input class="form-input" type="text">
         <label for="text">
-        <button type="submit">Buscar</button>
+        <button class="btn-enviar" type="submit">Buscar</button>
       </form>
     </div>
   </header>
