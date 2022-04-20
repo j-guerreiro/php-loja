d28adc9c39693bb1dba9e4f169cb1b8800deef24
j-guerreiro <joel.borghi@gmail.com>
2022-04-20 13:17:07 -0300

    feature escrevendo html e css do app

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Loja de Equipamentos Musicais</title>
 </head>
 <body>
+  <header>
+    <!-- formulario de busca -->
+    <div class="titulo">
+      <h1 class="titulo-interno">Loja de Instrumentos Musicais</h1>
+    </div>
+    <div class="form-busca">
+      <form class="form-interno" method="POST" action="buscar.php">
+        <input type="text">
+        <label for="text">
+        <button type="submit">Buscar</button>
+      </form>
+    </div>
+  </header>
+  <main></main>
+  <footer></footer>
   
 </body>
 </html>
